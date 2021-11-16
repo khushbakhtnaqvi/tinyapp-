d1684f56731ad88b7135913d33109a80dcbf6356
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-15 23:31:58 -0500

    modified the helper functions, clean up code, completed functionality

--- express_server.js
+++ express_server.js
@@ -1,19 +1,20 @@
-const { getUserByEmail } = require("./helpers")
+const { generateRandomString, getUserByEmail, urlsForUser } = require("./helpers") // helper functions
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session'); // for session cookies
 const bodyParser = require("body-parser");
-const bcrypt = require('bcryptjs');
-var morgan = require('morgan')
+const bcrypt = require('bcryptjs'); // for password encryption
+var morgan = require('morgan') // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(morgan("dev"));
+app.set("view engine", "ejs"); // ejs view engine
+app.use(morgan("dev")); // middleware
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
-}))
+})) // session cookies
 
+// URL Database
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -26,6 +27,7 @@ const urlDatabase = {
   }
 }
 
+// User Database
 const users = {
   "user1": {
     id: "user1",
@@ -39,18 +41,11 @@ const users = {
   }
 }
 
-function generateRandomString() {
-  let shortUrl = '';
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charsLen = chars.length;
-  for (var i = 0; i < 6; i++) {
-    shortUrl += chars.charAt(Math.floor(Math.random() * charsLen));
-  }
-  return shortUrl;
-}
-
+// get request and redirection upon getting root "/" endpoint
 app.get("/", (req, res) => {
   const id = req.session.user_id;
+
+  //check if user is logged in then redirect to "/urls" endpoint else redirect to login page
   if (id) {
     res.redirect("/urls");
   } else {
@@ -58,99 +53,111 @@ app.get("/", (req, res) => {
   }
 });
 
+//get request for "/register" endpoint
 app.get("/register", (req, res) => {
-  const id = req.session.user_id;
+  const id = req.session.user_id; // assigns session id to a user
   const templateVars = { user: users[id] };
   res.render("urls_register", templateVars);
 });
 
+// post request for "/register" endpoint
 app.post("/register", (req, res) => {
-  let { email, password } = req.body;
+  let { email, password } = req.body; // gets email and password enter by user for registeration
   if (email === '' || password === '') {
     res.status(400).send("Email and password should not be empty! Please <a href='/register'>try again</a>!");
     return;
   }
 
+  // calling function at registration time to check if email already exists
   if (getUserByEmail(email, users)) {
     res.status(400).send("Email already exists! Please <a href='/register'>try again</a>!");
     return;
   }
 
-  const id = generateRandomString();
-  password = bcrypt.hashSync(password, 8);
+  const id = generateRandomString();   // calling function to generate random string for unique user id
+  password = bcrypt.hashSync(password, 8); // password hashing
   users[id] = { id, email, password };
   const user = users[id];
-  req.session.user_id = user.id;
+  req.session.user_id = user.id; // assign user id to session
   res.redirect("/urls");
 
 });
 
-function urlsForUser(id) {
-  const cloneUrlDatabase = Object.assign({}, urlDatabase);
-  for (let key in cloneUrlDatabase) {
-    if (cloneUrlDatabase[key]["userID"] !== id) {
-      delete cloneUrlDatabase[key];
-    }
-  }
-  return cloneUrlDatabase;
-}
-
+// get request for "/urls" endpoint
 app.get("/urls", (req, res) => {
   const id = req.session.user_id;
+
+  // check if user is logged in
   if (!id) {
     res.status(403).send("User Not Logged In! Please <a href='/login'>login</a>!");
     return;
   }
 
-  const userUrlDatabase = urlsForUser(id);
+  // if user is logged go to "/urls" endpoint
+  const userUrlDatabase = urlsForUser(urlDatabase, id);
   const templateVars = { urls: userUrlDatabase, user: users[id] };
   res.render("urls_index", templateVars);
 });
 
+// get request for "/login" endpoint
 app.get("/login", (req, res) => {
   const id = req.session.user_id;
   const templateVars = { urls: urlDatabase, user: users[id] };
   res.render("urls_login", templateVars);
 });
 
+//post request for "/login" endpoint
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   const user = getUserByEmail(email, users);
+
+  // check if user does not exist
   if (!user) {
     res.status(403).send("Email cannot be found! Please <a href='/login'>try again</a>!");
     return;
   }
 
+  // check if there is a password mismatch
   if (!bcrypt.compareSync(password, user.password)) {
-    res.status(403).send("Password does not match! Please <a href='/login'>try again</a>!");
+    res.status(403).send("Wrong Password! Please <a href='/login'>try again</a>!");
     return;
   }
 
+  // if user exists and password matches, redirect to "/urls" endpoint
   req.session.user_id = user.id;
   res.redirect("/urls");
 });
 
+// post request for "/logout" endpoint
 app.post("/logout", (req, res) => {
   req.session = null
   res.redirect("/login");
 });
 
+// get request for "/urls/new" endpoint
 app.get("/urls/new", (req, res) => {
   const id = req.session.user_id;
+
+  // check if user is not logged in then redirect to "/login" endpoint
   if (!id) {
     res.redirect("/login");
     return;
   }
   const templateVars = { user: users[id] };
-  res.render("urls_new", templateVars);
+  res.render("urls_new", templateVars); // if user logged in then redirect to "/urls/new" endpoint
 });
 
+// post request for "/urls" endpoint
 app.post("/urls", (req, res) => {
   const id = req.session.user_id;
+
+  // check if user not logged in then throw error
   if (!id) {
     res.status(403).send("Access forbidden. User not logged in! Please <a href='/login'>login</a>!");
     return;
   }
+
+  // if user logged in then generate short URL id and redirect to "/url" endpoint
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = {};
@@ -159,71 +166,102 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + shortURL);
 });
 
+// redirects any user to entered shortURL
 app.get("/u/:shortURL", (req, res) => {
   const id = req.session.user_id;
   const shortURL = req.params.shortURL;
-  res.redirect(urlDatabase[shortURL]["longURL"]);
+
+  // if shortURL does not exist return error
+  if (!urlDatabase[shortURL]) {
+    res.status(403).send("URL does not exist!");
+    return;
+  }
+  // if shortURL exist redirect to the corresponding longURL
+  const longURL = urlDatabase[shortURL]['longURL'];
+  res.redirect(longURL);
 });
 
+// get request for "/urls/:shortURL" endpoint
 app.get("/urls/:shortURL", (req, res) => {
   const id = req.session.user_id;
+
+  // check if user is logged in
   if (!id) {
     res.status(403).send("User not logged in! Please <a href='/login'>login</a>!");
     return;
   }
 
   const shortURL = req.params.shortURL;
-  const userUrlDatabase = urlsForUser(id);
-  if (userUrlDatabase[shortURL]["userID"] !== id) {
+
+  // check if shortURL exists
+  if (!urlDatabase[shortURL]) {
+    res.status(403).send("URL does not exist!");
+    return;
+  }
+
+  // check if shortURL belongs to the logged in user
+  if (urlDatabase[shortURL]["userID"] != id) {
+
     res.status(403).send("Access forbidden. URL does not belong to logged in user!");
     return;
   }
 
+  // if user is logged in and shortURL belongs to user go to "/urls/:shortURL" endpoint
   const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]["longURL"], user: users[id] };
   res.render("urls_show", templateVars);
 });
 
+// post request to delete a shortURL
 app.post("/urls/:shortURL/delete", (req, res) => {
   const id = req.session.user_id;
+
+  // check if user is logged in
   if (!id) {
     res.status(403).send("User not logged in! Please <a href='/login'>login</a>!");
     return;
   }
 
+  // check if shortURL belongs to the logged in user
   const shortURL = req.params.shortURL;
-  const userUrlDatabase = urlsForUser(id);
-  if (userUrlDatabase[shortURL]["userID"] !== id) {
+  if (urlDatabase[shortURL]["userID"] !== id) {
     res.status(403).send("Access forbidden. URL does not belong to logged in user!");
     return;
   }
 
+  // if user is logged in and shortURL belongs to user delete the given shortURL
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
 
+// post request for updating the longURL against a shortURL
 app.post("/urls/:shortURL", (req, res) => {
   const id = req.session.user_id;
+
+  // check if shortURL belongs to the logged in user
   if (!id) {
     res.status(403).send("User not logged in! Please <a href='/login'>login</a>!");
     return;
   }
 
+  // check if shortURL belongs to the logged in user
   const shortURL = req.params.shortURL;
-  const userUrlDatabase = urlsForUser(id);
-  if (userUrlDatabase[shortURL]["userID"] !== id) {
+  if (urlDatabase[shortURL]["userID"] !== id) {
     res.status(403).send("Access forbidden. URL does not belong to logged in user!");
     return;
   }
 
+  // if user is logged in and shortURL belongs to user go to "/urls/:shortURL" endpoint
   const newLongURL = req.body.newLongURL;
   urlDatabase[shortURL]["longURL"] = newLongURL;
   res.redirect("/urls");
 });
 
+// if user enters anything else than the defined endpoints throw error
 app.get('*', (req, res) => {
   res.status(300).send("Page Does Not Exist !!!");
 });
 
+// server starts listening
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
--- helpers.js
+++ helpers.js
@@ -1,3 +1,15 @@
+// Function to generate random unique string for short URL id and unique user id
+function generateRandomString() {
+  let shortUrl = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charsLen = chars.length;
+  for (var i = 0; i < 6; i++) {
+    shortUrl += chars.charAt(Math.floor(Math.random() * charsLen));
+  }
+  return shortUrl;
+}
+
+// Function to get user details through the entered email
 function getUserByEmail(email, users) {
   for (let key in users) {
     if (users[key]["email"] === email) {
@@ -6,4 +18,18 @@ function getUserByEmail(email, users) {
   }
   return undefined;
 }
-module.exports = { getUserByEmail };
+
+// filters urls for the logged in user
+function urlsForUser(urlDatabase, id) {
+  console.log('Test27-in urlsForUser-urlDatabase: ');
+  console.log(urlDatabase);
+  const cloneUrlDatabase = Object.assign({}, urlDatabase);
+  for (let key in cloneUrlDatabase) {
+    if (cloneUrlDatabase[key]["userID"] !== id) {
+      delete cloneUrlDatabase[key];
+    }
+  }
+  return cloneUrlDatabase;
+}
+
+module.exports = { generateRandomString, getUserByEmail, urlsForUser };
